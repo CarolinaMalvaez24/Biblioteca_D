5491ebaee7456b26c395d2266f655c3afc88f2b8
Manis <joseaustin.z9439@gmail.com>
2022-11-13 01:15:33 -0600

    vistas

--- resources/views/autores/FormAutores.blade.php
+++ resources/views/autores/FormAutores.blade.php
@@ -13,22 +13,24 @@
       </div>
     </div>
   </div>
-  <div class="py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <form id="c_form-h" method="POST" action="{{url ('autores')}}">
-            @csrf
-            <div class="form-group row"><label class="col-2 @error('nombre_autor')is-invalid @enderror" value="{{old('nombre_autor')}}">
-            Nombre del Autor</label>
-                @error('nombre_autor')
-                  <div class="invalid-feedback">{{$message}}</div>
-                 @enderror
-              <div class="col-10"><input type="text" class="form-control" id="nombre_autor"  name="nombre_autor" placeholder="Nombre del autor del libro"></div>
-            </div><button type="submit" class="btn btn-primary">Agregar</button>
-          </form>
-        </div>
+  <div class="py-1">
+      <div class="container col-sm-4">
+          <div class="row">
+              <div class="d-flex justify-content-center card">
+                  <div class="card-body">
+                      <form id="c_form-h" method="POST" action="{{url ('autores')}}">
+                          @csrf
+                          <div class="form-group row"><label class="col-2 @error('nombre_autor')is-invalid @enderror" value="{{old('nombre_autor')}}">
+                                  Nombre del Autor</label>
+                              @error('nombre_autor')
+                              <div class="invalid-feedback">{{$message}}</div>
+                              @enderror
+                              <div class="col-10"><input type="text" class="form-control" id="nombre_autor"  name="nombre_autor" placeholder="Nombre del autor del libro"></div>
+                          </div><button type="submit" class="btn btn-primary">Agregar</button>
+                      </form>
+                  </div>
+              </div>
+          </div>
       </div>
-    </div>
   </div>
 @endsection
