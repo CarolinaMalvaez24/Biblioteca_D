1798529258758ed590ad14e3ae657ecbfc919fcf
Efren981 <efrenbarcenas981@gmail.com>
2022-11-14 12:03:51 -0600

    tabla asigna

---
+++ database/migrations/2022_10_30_041233_create_asigna_autores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asigna_autores', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_libro')->constrained('libros');
+            $table->foreignId('id_autores')->constrained('autores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asigna_autores');
+    }
+};
