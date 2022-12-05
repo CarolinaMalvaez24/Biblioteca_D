b80614840d241eb4a0697b2700e0146d4f10558e
Julio1205 <raptor.67.jL@gmail.com>
2022-12-04 21:24:05 -0600

    trigger

--- resources/views/libros/show.blade.php
+++ resources/views/libros/show.blade.php
@@ -8,9 +8,6 @@
          <h1>{{$libro->titulo}}</h1>
          <h1>{{$libro->anio}}</h1>
          <h1>{{$libro->descripcion}}</h1>
-         @foreach($libro as $categoria)
-         <h1>{{$libro->categorias as tipo_categoria}}</h1>
-        @endforeach
     @endforeach
     
 @endsection
