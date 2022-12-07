4693cd2b1612a3cd7348e928607074e009a457ba
Efren981 <efrenbarcenas981@gmail.com>
2022-12-06 20:35:44 -0600

    View

--- resources/views/libros/show.blade.php
+++ resources/views/libros/show.blade.php
@@ -4,27 +4,33 @@
     active
 @endsection
 @section("content")
-    <div>
+    <div class="container ">
+        <div class="row d-flex justify-content-center">
         @foreach($libros as $libro)
-         <h1>{{$libro->titulo}}</h1>
-         <h1>{{$libro->anio}}</h1>
-         <h1>{{$libro->descripcion}}</h1>
-         <h1>{{$libro->editoriales->nombre_editorial}}</h1>
-         @foreach($libro -> categorias as $categoria)
-            <div>
-                <h1>{{$categoria->tipo_categoria}}</h1>
-            </div>
-         @endforeach
+            <h1>Titulo</h1>
+             <h4>{{$libro->titulo}}</h4></br>
+            <h1>Año de publicacion</h1>
+             <h4>{{$libro->anio}}</h4>
+            <h1>Descripcion del libro</h1>
+             <h4>{{$libro->descripcion}}</h4>
+            <h1>Editorial del libro</h1>
+             <h4>{{$libro->editoriales->nombre_editorial}}</h4>
 
-         @foreach($libro -> autores as $autor)
-            <div>
-                <h1>{{$autor->nombre_autor}}</h1>
-            </div>
-         @endforeach
+            <h1>Categorias del libro</h1>
+             @foreach($libro -> categorias as $categoria)
+                <div>
+                    <h4>{{$categoria->tipo_categoria}}</h4>
+                </div>
+             @endforeach
 
-
-    @endforeach
+            <h1>Autores del libro</h1>
+             @foreach($libro -> autores as $autor)
+                <div>
+                    <h4>{{$autor->nombre_autor}}</h4>
+                </div>
+             @endforeach
+        @endforeach
+        </div>
     </div>
-    
     
 @endsection
