2c8b17626e8848cb474490c7b93799436c95d69a
Manis <joseaustin.z9439@gmail.com>
2022-12-10 22:27:32 -0600

    vista view libro

--- resources/views/libros/show.blade.php
+++ resources/views/libros/show.blade.php
@@ -6,30 +6,34 @@
 @section("content")
     <div class="container ">
         <div class="row d-flex justify-content-center">
-        @foreach($libros as $libro)
-            <h1>Titulo</h1>
-             <h4>{{$libro->titulo}}</h4></br>
-            <h1>Año de publicacion</h1>
-             <h4>{{$libro->anio}}</h4>
-            <h1>Descripcion del libro</h1>
-             <h4>{{$libro->descripcion}}</h4>
-            <h1>Editorial del libro</h1>
-             <h4>{{$libro->editoriales->nombre_editorial}}</h4>
-
-            <h1>Categorias del libro</h1>
-             @foreach($libro -> categorias as $categoria)
-                <div>
-                    <h4>{{$categoria->tipo_categoria}}</h4>
+            <div
+            <div class="pt-5" style="">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4 mr-auto order-2 order-md-1"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-mobile.svg"> </div>
+                    <div class="px-md-5 p-3 d-flex flex-column align-items-start justify-content-center col-md-7 order-1 order-md-2" style="">
+                        <div class="row">
+                            <div class="col-md-12" style="">
+                            <h1 class="" style="">Titulo del libro</br>
+                            <span class="badge badge-light"></span></h1>
+                            {{$libro->titulo}}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-10">
+                                <h4 style="" class="">Año de publicacion</h4>
+                                {{$libro->anio}}
+                            </div>
+                            <div class="col-2">
+                                manis
+                            </div>
+                        </div>
+                      <h3 class="mx-auto" style="">Heading 3 <span class="badge badge-light"> New</span></h3>
+                      <p class="mb-3 lead" style="" >And yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface.</p>
+                    </div>
+                  </div>
                 </div>
-             @endforeach
-
-            <h1>Autores del libro</h1>
-             @foreach($libro -> autores as $autor)
-                <div>
-                    <h4>{{$autor->nombre_autor}}</h4>
-                </div>
-             @endforeach
-        @endforeach
+              </div>
         </div>
     </div>
     
