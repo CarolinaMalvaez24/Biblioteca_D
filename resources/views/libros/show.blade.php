9491e1db851b6db3e9de8ccbbf8fc738cc966f57
Manis <joseaustin.z9439@gmail.com>
2022-12-10 23:09:42 -0600

    vistas libros

--- resources/views/libros/show.blade.php
+++ resources/views/libros/show.blade.php
@@ -6,10 +6,10 @@
 @section("content")
     <div class="container ">
         <div class="row d-flex justify-content-center">
-            <div
             <div class="pt-5" style="">
                 <div class="container">
                   <div class="row">
+                    @foreach($libros as $libro)
                     <div class="col-md-4 mr-auto order-2 order-md-1"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-mobile.svg"> </div>
                     <div class="px-md-5 p-3 d-flex flex-column align-items-start justify-content-center col-md-7 order-1 order-md-2" style="">
                         <div class="row">
@@ -19,22 +19,43 @@
                             {{$libro->titulo}}
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-10">
-                                <h4 style="" class="">Año de publicacion</h4>
+                        <div class="my-3 row d-flex">
+                            <div class="my-3 col-10">
+                                <h4 class="">Año de publicacion</h4>
                                 {{$libro->anio}}
                             </div>
-                            <div class="col-2">
-                                manis
+                            <div class="my-3 col-2">
+                                <h4 style="" class="">Editorial</h4>
+                                {{$libro->editoriales->nombre_editorial}}
                             </div>
                         </div>
-                      <h3 class="mx-auto" style="">Heading 3 <span class="badge badge-light"> New</span></h3>
-                      <p class="mb-3 lead" style="" >And yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface.</p>
+                        <div class="mx-auto">
+                            <h3 class="mx-auto" style="">Descripcion del libro <span class="badge badge-light"></span></h3>
+                            <p class="mb-3 lead" style="">{{$libro->descripcion}}</p>
+                        </div>
+                        <div class="row d-flex">
+                            <div class="col-10">
+                                <h4 style="" class="">Autores</h4>
+                                @foreach($libro -> autores as $autor)
+                                <div>
+                                    {{$autor->nombre_autor}}
+                                </div>
+                             @endforeach
+                            </div>
+                            <div class="col-2">
+                                <h4 style="" class="">Categoria</h4>
+                                @foreach($libro -> categorias as $categoria)
+                                <div>
+                                    {{$categoria->tipo_categoria}}
+                                </div>
+                             @endforeach
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                  @endforeach
                 </div>
-              </div>
+                </div>
+            </div>
         </div>
     </div>
-    
 @endsection
