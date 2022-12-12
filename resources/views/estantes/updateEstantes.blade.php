fa516f1663f33ca6ef562fda0de7a5731735fda4
Efren981 <efrenbarcenas05@gmail.com>
2022-12-12 10:50:14 -0600

    prestamo

--- resources/views/estantes/updateEstantes.blade.php
+++ resources/views/estantes/updateEstantes.blade.php
@@ -17,14 +17,14 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12" style="">
-          <form id="c_form-h" method="POST" action="{{route('prestamos.update',$estante->id)}}">
+          <form id="c_form-h" method="POST" action="{{route('prestamos.update',$prestamo->id)}}">
             @csrf
             @method('PUT')
             <div class="form-group row"><label class="col-2">Usuario</label>
               <div class="col-3"><select class="form-select @error('id_usuarios')is-invalid @enderror" aria-label="Default select example" id="id_usuarios" name="id_usuarios">
                   <option selected="" value="Open this select menu">Elegir nombre de usuario</option>
                   @foreach($usuarios as $usuario)
-                    <option value="{{$usuario->id}}" {{$usuario->id== $estante->id_usuarios ? 'selected' : ''}}>{{$usuario->nombreUsuario}}</option>
+                    <option value="{{$usuario->id}}" {{$usuario->id== $prestamo->users_id ? 'selected' : ''}}>{{$usuario->name}}</option>
                   @endforeach
                 </select>
                 @error('id_usuarios')
@@ -36,7 +36,7 @@
               <div class="col-3"><select class="form-select @error('id_libros')is-invalid @enderror" aria-label="Default select example" id="id_libros" name="id_libros">
                   <option selected="" value="Open this select menu">Selecciona libro</option>
                   @foreach($libros as $libro)
-                    <option value="{{$libro->id}}" {{$libro->id== $estante->id_libros ? 'selected' : ''}}>{{$libro->descripcion}}</option>
+                    <option value="{{$libro->id}}" {{$libro->id== $prestamo->libros_id ? 'selected' : ''}}>{{$libro->titulo}}</option>
                   @endforeach
                 </select>
                 @error('id_libros')
